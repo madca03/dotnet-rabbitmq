4f4576de83ae1c62e12700cb08ad07a82cd220fe
Mark Allen Agaton <markallenagaton03@gmail.com>
2021-01-20 10:44:40 +0800

    3-publish-subscribe

---
+++ trial1/3-publish-subscribe/EmitLog/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace EmitLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() {HostName = "localhost"};
+            using var connection = factory.CreateConnection("3-publish-subscribe-emit-log");
+            using (var channel = connection.CreateModel())
+            {
+                var exchangeName = "logs";
+                channel.ExchangeDeclare(exchangeName, ExchangeType.Fanout);
+
+                var message = GetMessage(args);
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: exchangeName,
+                    routingKey: "",
+                    basicProperties: null,
+                    body: body);
+
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+        }
+    }
+}
---
+++ trial1/3-publish-subscribe/ReceiveLog/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace ReceiveLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() {HostName = "localhost"};
+            using var connection = factory.CreateConnection("3-publish-subscribe-receiver");
+            using (var channel = connection.CreateModel())
+            {
+                var exchangeName = "logs";
+                channel.ExchangeDeclare(exchangeName, ExchangeType.Fanout);
+
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+                var queue = channel.QueueDeclare();
+                var queueName = queue.QueueName;
+
+                channel.QueueBind(queue: queueName,
+                    exchange: exchangeName,
+                    routingKey: "");
+
+                Console.WriteLine(" [*] Waiting for logs");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += OnReceive;
+                channel.BasicConsume(queueName, false, consumer);
+
+                Console.WriteLine(" Press [enter] to exit");
+                Console.ReadLine();
+            }
+        }
+
+        private static void OnReceive(object model, BasicDeliverEventArgs ea)
+        {
+            var channel = ((EventingBasicConsumer) model).Model;
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine(" [x] {0}", message);
+            channel.BasicAck(ea.DeliveryTag, false);
+        }
+    }
+}
